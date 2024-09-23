dbc1f0eb0b43f86e9e196a01c2d2b5ca80b47315
micael__wh <142758143+DJarana911202323@users.noreply.github.com>
2024-09-23 12:26:07 -0300

    pç´´[pl

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-let nomeusuario="micael,amanda";
-alert(nomeUsuario)
-
-
-
-
+//let nomeUsuario='Micael,Amanda';
+//alert(nomeUsuario)
+function soma(num1,num2){
+    return num1+num2;
+}
+console.log(soma(1,2));
+console.log(3*3)
+function areQuadrado
+(lado){
+    return lado**2;
+}
+console.log(areQuadrado(6));
+let areTriangulo= function(lado)
+     return (base*altura)/2;
+}
+console.log(areTriangulo(6))
+let nome= parseInt(promt('qual e a area do quadrado'))
+document.write(name);
+function soma(num3,num4,num5){
+ return soma/3
+}
+console.log(nome)
