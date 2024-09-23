dcb2e7b487ef518a2f062728ce8a2d7cfe0ad023
micael__wh <142758143+DJarana911202323@users.noreply.github.com>
2024-09-23 17:48:30 -0300

    ihu

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-//let nomeUsuario='Micael,Amanda';
-//alert(nomeUsuario)
+// let nomeUsuario='Micael,Amanda';
+// alert(nomeUsuario)
 function soma(num1,num2){
     return num1+num2;
 }
-console.log(soma(1,2));
-console.log(3*3)
-function areQuadrado
+console.log (soma(1,2));
+console.log ( 3*3)
+function areaQuadrado
 (lado){
     return lado**2;
 }
-console.log(areQuadrado(6));
-let areTriangulo= function(lado)
-     return (base*altura)/2;
+console.log(areaQuadrado(6));
+let areaTriangulo= function(base,altura){
+    return(base*altura)/2;
 }
-console.log(areTriangulo(6))
-let nome= parseInt(promt('qual e a area do quadrado'))
+console.log(areaTriangulo)
+let nome= parseInt(prompt('qual a média dos valores?'))
 document.write(nome);
 function soma(num3,num4,num5){
- return soma/3
+    return soma/3
 }
 console.log(nome)
