93b5c1c23f1fac040c6ae082b9e18ea44adf8c2f
micael__wh <142758143+DJarana911202323@users.noreply.github.com>
2024-09-30 11:35:42 -0300

    khjkjhgj

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-// let nomeUsuario='Micael,Amanda';
-// alert(nomeUsuario)
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log (soma(1,2));
-console.log ( 3*3)
-function areaQuadrado
-(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(6));
-let areaTriangulo= function(base,altura){
-    return(base*altura)/2;
-}
-console.log(areaTriangulo)
-let nome= parseInt(prompt('qual a média dos valores?'))
-document.write(nome);
-function soma(num3,num4,num5){
-    return soma/3
-}
-console.log(nome)
+// // let nomeUsuario='Amanda,Micael';
+// // alert(nomeUsuario)
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log (soma(1,2));
+// console.log ( 3*3)
+// function areaQuadrado
+// (lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6));
+// let areaTriangulo= function(base,altura){
+//     return(base*altura)/2;
+// }
+// console.log(areaTriangulo)
+// let num1=parseInt(prompt('numero'));
+// let num2=parseInt(prompt('numero'));
+// let num3=parseInt(prompt('numero'));
+// function media (x,y,z){
+//      return (x=y=z)/3;
+let estados={'Parana','RJ','Acre','Alagoas','Amapa','Amaonas','Bahia','Ceara','Espirito Santos',''}
